1bfce98e36deabfe8c924061ab3af930a6c8ba77
SierraDLC <100964979+SierraDLC@users.noreply.github.com>
2022-06-07 14:32:02 -0400

    nice

---
+++ src/main/java/CrudProject/Repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package CrudProject.Repository;
+
+import CrudProject.Model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
---
+++ src/main/java/CrudProject/Service/EmployeeService.java
@@ -0,0 +1,16 @@
+package CrudProject.Service;
+
+import java.util.List;
+
+import CrudProject.Model.Employee;
+import org.springframework.data.domain.Page;
+
+
+
+public interface EmployeeService {
+    List <Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+    Page < Employee > findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+}
